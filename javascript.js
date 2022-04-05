7889fe5b5ea9aeeda60b146ac5efa87f3a2d134b
nairse <nairse@mail.uc.edu>
2022-04-04 22:33:21 -0400

    Created a function to play one round of the game

--- javascript.js
+++ javascript.js
@@ -14,7 +14,27 @@ function computerPlay(){
     }
     else {
         console.log(rando);
-        return "Scissors";
+        return "Scissor";
     }   
 }
 
+let computerSelection = computerPlay();
+let playerSelection = prompt("Choose Your Weapon: Rock, Paper or Scissor?");
+playerSelection = playerSelection.toLowerCase();
+playerSelection = playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
+
+
+function oneRound(compSel, playrSel){
+    if((playrSel == "Rock" && compSel == "Paper") || (playrSel == "Paper" && compSel == "Scissor") || (playrSel == "Scissor" && compSel == "Rock")){
+        return alert(`You Lose!, ${computerSelection} beats ${playerSelection}!`);
+    }
+    else if  ((playrSel == "Rock" && compSel == "Scissor") || (playrSel == "Paper" && compSel == "Rock") || (playrSel == "Scissor" && compSel == "Paper")){   
+        return alert(`You Win!, ${playerSelection} beats ${computerSelection}!`);
+}
+    else {
+        return alert("It's a tie... an unstoppable force met an immovable object!");
+    }
+
+}
+
+oneRound(computerSelection, playerSelection);
