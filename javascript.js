3dbd926070a16efbe9247ee52e797ad6ed8f2b3f
nairse <nairse@mail.uc.edu>
2022-04-04 23:37:51 -0400

    Added a game function that determines winner after 5 rounds of play

--- javascript.js
+++ javascript.js
@@ -1,40 +1,64 @@
 
 let rando;
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay(){
     rando = Math.floor(Math.random()*100);
     if(rando >=0 && rando < 33){
-        console.log(rando);
         return "Rock";
         
     }
     else if (rando >= 33 && rando < 66){
-        console.log(rando);
         return "Paper";
     }
     else {
-        console.log(rando);
         return "Scissor";
-    }   
-}
-
-let computerSelection = computerPlay();
-let playerSelection = prompt("Choose Your Weapon: Rock, Paper or Scissor?");
-playerSelection = playerSelection.toLowerCase();
-playerSelection = playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
-
-
+    }
+}   
 function oneRound(compSel, playrSel){
     if((playrSel == "Rock" && compSel == "Paper") || (playrSel == "Paper" && compSel == "Scissor") || (playrSel == "Scissor" && compSel == "Rock")){
-        return alert(`You Lose!, ${computerSelection} beats ${playerSelection}!`);
+        compScore();
+        return console.log(`You Lose!, ${compSel} beats ${playrSel}!`);
     }
     else if  ((playrSel == "Rock" && compSel == "Scissor") || (playrSel == "Paper" && compSel == "Rock") || (playrSel == "Scissor" && compSel == "Paper")){   
-        return alert(`You Win!, ${playerSelection} beats ${computerSelection}!`);
+        playrScore();
+        return console.log(`You Win!, ${playrSel} beats ${compSel}!`);
 }
     else {
-        return alert("It's a tie... an unstoppable force met an immovable object!");
+        return console.log("It's a tie... an unstoppable force met an immovable object!");
     }
 
 }
 
-oneRound(computerSelection, playerSelection);
+function game(){
+    for (let i = 0; i < 5; i++){
+       let computerSelection = computerPlay();
+       let playerSelection = prompt("Choose Your Weapon: Rock, Paper or Scissor?");
+        playerSelection = playerSelection.toLowerCase();
+        playerSelection = playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
+        oneRound(computerSelection, playerSelection);
+        console.log(`Round ${i+1}: ` + "computer score is " + computerScore + ", " + "player score is " + playerScore);
+    }
+    
+    
+    if (computerScore > playerScore){
+        console.log("Computer is the Winner!");
+    }
+    else if (playerScore > computerScore) {
+        console.log("You are the winner! Get ready for chicken dinner!");
+    }
+
+    else console.log("Game Tied...weak -_-");
+}
+
+
+game();
+
+function compScore(){
+    computerScore += 1;
+}
+
+function playrScore(){
+    playerScore += 1;
+}
