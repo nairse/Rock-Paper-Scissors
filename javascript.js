d3575a06f480706c606a83eff1986107b70723b5
nairse <nairse@mail.uc.edu>
2022-04-05 20:56:45 -0400

    Added check for incorrect user selection, refactored code

--- javascript.js
+++ javascript.js
@@ -3,11 +3,25 @@ let rando;
 let computerScore = 0;
 let playerScore = 0;
 
+// function declaration for player's item selection
+function playerPlay(){
+        let weapon = prompt("Choose Your Weapon: Rock, Paper or Scissor?");
+        weapon = weapon.toLowerCase();
+        weapon = weapon.replace(weapon[0],weapon[0].toUpperCase());
+        if ((weapon == "Rock") || (weapon == "Paper") || (weapon == "Scissor")){
+            return weapon;
+        } 
+        else {
+            alert("Weapon not permitted. Please choose either a Rock, Paper or Scissor");
+            playerPlay();
+        }    
+}
+
+//function declaration for computer's item selection at random
 function computerPlay(){
     rando = Math.floor(Math.random()*100);
     if(rando >=0 && rando < 33){
         return "Rock";
-        
     }
     else if (rando >= 33 && rando < 66){
         return "Paper";
@@ -15,7 +29,9 @@ function computerPlay(){
     else {
         return "Scissor";
     }
-}   
+}
+
+//function declaration that plays one round of the game
 function oneRound(compSel, playrSel){
     if((playrSel == "Rock" && compSel == "Paper") || (playrSel == "Paper" && compSel == "Scissor") || (playrSel == "Scissor" && compSel == "Rock")){
         compScore();
@@ -28,19 +44,17 @@ function oneRound(compSel, playrSel){
     else {
         return console.log("It's a tie... an unstoppable force met an immovable object!");
     }
-
 }
 
+//function declaration that plays a set of 5 games and decides the winner
 function game(){
     for (let i = 0; i < 5; i++){
        let computerSelection = computerPlay();
-       let playerSelection = prompt("Choose Your Weapon: Rock, Paper or Scissor?");
-        playerSelection = playerSelection.toLowerCase();
-        playerSelection = playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase());
+       let playerSelection = playerPlay();
+       
         oneRound(computerSelection, playerSelection);
         console.log(`Round ${i+1}: ` + "computer score is " + computerScore + ", " + "player score is " + playerScore);
     }
-    
     
     if (computerScore > playerScore){
         console.log("Computer is the Winner!");
@@ -53,12 +67,14 @@ function game(){
 }
 
 
-game();
-
+//function to keep track of computer's score
 function compScore(){
     computerScore += 1;
 }
 
+//function to keep track of player's score
 function playrScore(){
     playerScore += 1;
 }
+
+game();
